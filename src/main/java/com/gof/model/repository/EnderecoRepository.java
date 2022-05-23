26f4da333e3ce874dc9865e466200cedc53ee5f9
Daniel Pereira <daniel.si@outlook.com.br>
2022-05-23 16:03:31 -0300

    Criando repositório de endereço.

---
+++ src/main/java/com/gof/model/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.gof.model.repository;
+
+import com.gof.model.domain.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, String> {
+}
