d58c1342b3c08ddb39de8e92e316bf322029e196
Daniel Pereira <daniel.si@outlook.com.br>
2022-05-23 16:00:19 -0300

    Criando entidades de cliente e endereço.

---
+++ src/main/java/com/gof/model/domain/Endereco.java
@@ -0,0 +1,29 @@
+package com.gof.model.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    @Id
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
+}
