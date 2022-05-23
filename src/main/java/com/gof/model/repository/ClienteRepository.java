cf579652c332040c3b618d47e681f2365a554a94
Daniel Pereira <daniel.si@outlook.com.br>
2022-05-23 16:02:41 -0300

    Criando repositório de cliente.

---
+++ src/main/java/com/gof/model/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.gof.model.repository;
+
+import com.gof.model.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
